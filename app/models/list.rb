b31c6fb7f485b873103f7382054b8d19db00e179
Babajide Muritala <jide142000@yahoo.com>
2023-02-07 18:49:11 +0100

    model validations all green

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,6 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks
-  has_many :movies, dependent: :destroy
+  has_many :movies, through: :bookmarks, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 end
