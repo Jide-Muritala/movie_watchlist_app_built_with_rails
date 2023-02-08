758caf5e7ab89c347cdeb051340dc584a0a70e2d
Babajide Muritala <jide142000@yahoo.com>
2023-02-08 18:02:32 +0100

    nested bookmark resources and generated bookmark controller

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,4 @@
+class BookmarksController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # List routes
   get "lists", to: "lists#index"
   get "lists/new", to: "lists#new"
   get "lists/:id", to: "lists#show", as: :list
   post "lists", to: "lists#create"
 
-
+  # Bookmark routes
+  resources :lists do
+    resources :bookmarks, only: [:new]
+  end
 end
