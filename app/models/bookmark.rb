ef8df8c51a0d5915400d98a475baf7554f1b8329
Babajide Muritala <jide142000@yahoo.com>
2023-02-08 08:22:45 +0100

    modified schema

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, length: { minimum: 6 }
+  validates :comment, length: { minimum: 6 }, allow_blank: true
   validates :movie_id, uniqueness: { scope: :list_id }, allow_nil: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_140002) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_071934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_140002) do
     t.string "title"
     t.text "overview"
     t.text "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
