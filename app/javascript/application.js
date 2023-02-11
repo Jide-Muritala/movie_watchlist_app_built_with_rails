0b0cb860b159e9cf11984d9f059527f8ae7e5829
Babajide Muritala <jide142000@yahoo.com>
2023-02-11 15:52:15 +0100

    add images and update frontend

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,4 @@
-import { Application } from "@hotwired/stimulus"
-
-const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import "bootstrap";
