e1c98fae895d6a6549f8e96b688f9c93e2271139
Babajide Muritala <jide142000@yahoo.com>
2023-02-11 12:22:53 +0100

    install star_rating and generate its controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,9 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
-import "bootstrap";
+import { Application } from "@hotwired/stimulus"
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+
+export { application }
